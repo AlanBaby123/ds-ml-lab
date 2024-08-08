2755c70e4529d5ea2cee6148e6ae05d7e99093e2
AlanBaby123 <159029922+AlanBaby123@users.noreply.github.com>
2024-08-08 11:12:36 +0530

    Create bubble.py

---
+++ bubble.py
@@ -0,0 +1,29 @@
+ls=[]
+n=int(input("Enter limit:"))
+for i in range(n):
+    ls.append(int(input("Enter element: ")))
+print("Unsorted list: ",ls)
+for i in range(n):
+    for j in range(i+1,n):
+        if ls[i]>ls[j]:
+            temp=ls[i]
+            ls[i]=ls[j]
+            ls[j]=temp
+print("Sorted list",ls)
+
+
+output
+
+mlm@mlm-OptiPlex-3020:~$ python3 buble.py
+Enter limit:4
+Enter element: 2           
+Enter element: 6
+Enter element: 3
+Enter element: 5
+Unsorted list:  [2, 6, 3, 5]
+Sorted list [2, 3, 5, 6]
+mlm@mlm-OptiPlex-3020:~$ 
+
+
+
+
